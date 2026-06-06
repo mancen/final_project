f618b280cedb1f51bc5e624b8689943b968c15d3
mancen <mancen616@gmail.com>
2026-06-06 17:24:19 +0800

    add article.html

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const navList = document.querySelector(".nav-list"); // 主導覽選單元素
 // 事件監聽器（白話翻譯版：當使用者點擊漢堡選單按鈕，我要做什麼事情）
 menuBtn.addEventListener("click", function () {
     navList.classList.toggle("active"); // 我要切換主導覽選單元素上的 class，如果有 active 就移除，沒有就加上
+    document.body.classList.toggle("menu-open"); // 我要切換 body 元素上的 class，如果有 menu-open 就移除，沒有就加上
 });
